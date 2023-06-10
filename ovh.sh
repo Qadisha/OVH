12c1d0050f80861ef04e7116b371d234d0402415
Antonino Esposito <antonino.esposito@infocube.it>
2023-06-10 15:15:14 +0200

    Execute the update API to commit changes

--- ovh.sh
+++ ovh.sh
@@ -4,15 +4,15 @@ OVH_CONSUMER_KEY=""
 OVH_APP_KEY=""
 OVH_APP_SECRET=""
 DOMAIN=""
-TIME=$(curl -s https://api.ovh.com/1.0/auth/time)
-CLEAR_SIGN="$OVH_APP_SECRET+$OVH_CONSUMER_KEY+$HTTP_METHOD+$HTTP_QUERY+$HTTP_BODY+$TIME"
-SIG='$1$'$(echo -n $CLEAR_SIGN | openssl dgst -sha1 | sed -e 's/^.* //')
 
 
 # Get existing DNS
 HTTP_METHOD="GET"
 HTTP_QUERY="https://api.ovh.com/1.0/domain/$DOMAIN/nameServer"
 HTTP_BODY=''
+TIME=$(curl -s https://api.ovh.com/1.0/auth/time)
+CLEAR_SIGN="$OVH_APP_SECRET+$OVH_CONSUMER_KEY+$HTTP_METHOD+$HTTP_QUERY+$HTTP_BODY+$TIME"
+SIG='$1$'$(echo -n $CLEAR_SIGN | openssl dgst -sha1 | sed -e 's/^.* //')
 
 NSSERVERS=$(curl -X $HTTP_METHOD -H "Content-Type:application/json;charset=utf-8"  -H "X-Ovh-Application:$OVH_APP_KEY" -H "X-Ovh-Timestamp:$TIME" -H "X-Ovh-Signature:$SIG" -H "X-Ovh-Consumer:$OVH_CONSUMER_KEY" --data "$HTTP_BODY" $HTTP_QUERY )
 
@@ -20,6 +20,9 @@ NSSERVERS=$(curl -X $HTTP_METHOD -H "Content-Type:application/json;charset=utf-8
 HTTP_METHOD="POST"
 HTTP_QUERY="https://api.ovh.com/1.0/domain/$DOMAIN/nameServer"
 HTTP_BODY="{\"nameServer\": [{\"host\": \"ken.ns.cloudflare.com\"},{\"host\": \"nina.ns.cloudflare.com\"}]}"
+TIME=$(curl -s https://api.ovh.com/1.0/auth/time)
+CLEAR_SIGN="$OVH_APP_SECRET+$OVH_CONSUMER_KEY+$HTTP_METHOD+$HTTP_QUERY+$HTTP_BODY+$TIME"
+SIG='$1$'$(echo -n $CLEAR_SIGN | openssl dgst -sha1 | sed -e 's/^.* //')
 
 curl -X $HTTP_METHOD -H "Content-Type:application/json;charset=utf-8"  -H "X-Ovh-Application:$OVH_APP_KEY" -H "X-Ovh-Timestamp:$TIME" -H "X-Ovh-Signature:$SIG" -H "X-Ovh-Consumer:$OVH_CONSUMER_KEY" --data "$HTTP_BODY" $HTTP_QUERY
 
@@ -29,7 +32,7 @@ curl -X $HTTP_METHOD -H "Content-Type:application/json;charset=utf-8"  -H "X-Ovh
 for i in $(echo $NSSERVERS | sed 's/\[//g; s/\]//g' | tr "," "\n")
 do
    HTTP_METHOD='DELETE'
-   HTTP_QUERY='https://api.ovh.com/1.0/domain/impreseunite.net/nameServer/'$i
+   HTTP_QUERY='https://api.ovh.com/1.0/domain/$DOMAIN/nameServer/'$i
    TIME=$(curl -s https://api.ovh.com/1.0/auth/time)
    CLEAR_SIGN="$OVH_APP_SECRET+$OVH_CONSUMER_KEY+$HTTP_METHOD+$HTTP_QUERY+$HTTP_BODY+$TIME"
    SIG='$1$'$(echo -n $CLEAR_SIGN | openssl dgst -sha1 | sed -e 's/^.* //')
@@ -37,4 +40,14 @@ do
    DELETENSSERVER=$(curl -X $HTTP_METHOD -H "Content-Type:application/json;charset=utf-8"  -H "X-Ovh-Application:$OVH_APP_KEY" -H "X-Ovh-Timestamp:$TIME" -H "X-Ovh-Signature:$SIG" -H "X-Ovh-C>
    echo $DELETENSSERVER
 done
+
+# Update DNS
+HTTP_METHOD="POST"
+HTTP_QUERY="https://api.ovh.com/1.0/domain/$DOMAIN/nameServer/update"
+HTTP_BODY=''
+TIME=$(curl -s https://api.ovh.com/1.0/auth/time)
+CLEAR_SIGN="$OVH_APP_SECRET+$OVH_CONSUMER_KEY+$HTTP_METHOD+$HTTP_QUERY+$HTTP_BODY+$TIME"
+SIG='$1$'$(echo -n $CLEAR_SIGN | openssl dgst -sha1 | sed -e 's/^.* //')
+
+curl -X $HTTP_METHOD -H "Content-Type:application/json;charset=utf-8"  -H "X-Ovh-Application:$OVH_APP_KEY" -H "X-Ovh-Timestamp:$TIME" -H "X-Ovh-Signature:$SIG" -H "X-Ovh-Consumer:$OVH_CONSUMER_KEY" --data "$HTTP_BODY" $HTTP_QUERY
 
