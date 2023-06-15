b5d280150de81bb9def01fd85f5607b8e4c79966
Antonino Esposito <antonino.esposito@infocube.it>
2023-06-15 17:44:29 +0200

    Set OVH domain as external

--- ovh.sh
+++ ovh.sh
@@ -16,6 +16,18 @@ SIG='$1$'$(echo -n $CLEAR_SIGN | openssl dgst -sha1 | sed -e 's/^.* //')
 
 NSSERVERS=$(curl -X $HTTP_METHOD -H "Content-Type:application/json;charset=utf-8"  -H "X-Ovh-Application:$OVH_APP_KEY" -H "X-Ovh-Timestamp:$TIME" -H "X-Ovh-Signature:$SIG" -H "X-Ovh-Consumer:$OVH_CONSUMER_KEY" --data "$HTTP_BODY" $HTTP_QUERY )
 
+# Set domain as external
+HTTP_METHOD="PUT"
+HTTP_QUERY="https://api.ovh.com/1.0/domain/$DOMAIN"
+HTTP_BODY="{\"nameServerType\":\"external\"  }"
+TIME=$(curl -s https://api.ovh.com/1.0/auth/time)
+CLEAR_SIGN="$OVH_APP_SECRET+$OVH_CONSUMER_KEY+$HTTP_METHOD+$HTTP_QUERY+$HTTP_BODY+$TIME"
+SIG='$1$'$(echo -n $CLEAR_SIGN | openssl dgst -sha1 | sed -e 's/^.* //')
+
+curl -X $HTTP_METHOD -H "Content-Type:application/json;charset=utf-8"  -H "X-Ovh-Application:$OVH_APP_KEY" -H "X-Ovh-Timestamp:$TIME" -H "X-Ovh-Signature:$SIG" -H "X-Ovh-Consumer:$OVH_CONSUMER_KEY" --data "$HTTP_BODY" $HTTP_QUERY
+
+
+
 # Add Cloudflare DNS
 HTTP_METHOD="POST"
 HTTP_QUERY="https://api.ovh.com/1.0/domain/$DOMAIN/nameServer"
@@ -32,7 +44,8 @@ curl -X $HTTP_METHOD -H "Content-Type:application/json;charset=utf-8"  -H "X-Ovh
 for i in $(echo $NSSERVERS | sed 's/\[//g; s/\]//g' | tr "," "\n")
 do
    HTTP_METHOD='DELETE'
-   HTTP_QUERY='https://api.ovh.com/1.0/domain/$DOMAIN/nameServer/'$i
+   HTTP_QUERY='https://api.ovh.com/1.0/domain/'$DOMAIN'/nameServer/'$i
+   HTTP_BODY=''
    TIME=$(curl -s https://api.ovh.com/1.0/auth/time)
    CLEAR_SIGN="$OVH_APP_SECRET+$OVH_CONSUMER_KEY+$HTTP_METHOD+$HTTP_QUERY+$HTTP_BODY+$TIME"
    SIG='$1$'$(echo -n $CLEAR_SIGN | openssl dgst -sha1 | sed -e 's/^.* //')
